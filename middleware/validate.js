a8333fdecd12096694f82be14c45b53dceae597f
Jackie <brundtoe@outlook.dk>
2020-06-28 17:22:36 +0200

    tilføjet action put

--- middleware/validate.js
+++ middleware/validate.js
@@ -6,12 +6,26 @@ module.exports = {
   post: (req, res, next) => {
 
     try {
-      Joi.assert(req.body,userSchema)
+      const schema = userSchema
+        .with('username', 'birth_year')
+        .xor('password', 'access_token')
+        .with('password', 'repeat_password')
+      Joi.assert(req.body, schema)
       next()
     } catch (err) {
-      next(createError(400,err))
+      next(createError(400, err))
     }
-  }
+  },
+  put: (req, res, next) => {
+
+    try {
+      Joi.assert(req.body, userSchema)
+      next()
+    } catch (err) {
+      next(createError(400, err))
+    }
+  },
+
 }
 
 //      const {error, value } = schema.validate({ username: 'abc', birth_year: 1994, password: 'secret', repeat_password: 'secret', email: 'jens@mail.com'})
