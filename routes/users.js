f928eab42d4bb017b88b3ac3ef129c63c9a98ef9
Jackie <brundtoe@outlook.dk>
2020-06-29 14:07:02 +0200

    eksempel med mongodb connection fra dbs/mystore

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const router = express.Router()
 const validate = require('../middleware/validate')
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
-})
+const conn = require('../dbs/mystore')
+const assert = require('assert')
 
 router.route('/')
-  .get((req, res) => {
-    res.send('respond with a resource')
+  .get(async (req, res, next) => {
+    try {
+      const db = await conn()
+      let r = await db.collection('testing').insertOne(
+        {
+          firstname: 'Katrine',
+          lastname: 'Svendsen',
+          email: 'katrine@mail.com',
+          created_at: new Date(),
+          updated_at: null
+        }
+      )
+      assert(1, r.insertedCount)
+      res.send('respond with a resource')
+    } catch (err) {
+      next(err)
+    }
+
   })
   .post(validate.post, (req, res) => {
     res.status(201).json(req.body)
