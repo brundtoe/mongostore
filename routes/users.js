b5a869a10b194aad99b1de7dc7b2211f42bf2e4b
Jackie <brundtoe@outlook.dk>
2020-06-29 17:49:51 +0200

    get users returnerer nu udvalgte users

--- routes/users.js
+++ routes/users.js
@@ -3,29 +3,37 @@ const router = express.Router()
 const validate = require('../middleware/validate')
 const mongoCon = require('../dbs')
 const assert = require('assert')
+const userCollection = 'users'
 
 router.route('/')
   .get(async (req, res, next) => {
 
     try {
       let db = mongoCon.getConnection()
-      let r = await db.collection('testing').insertOne(
-        {
-          firstname: 'Katrine',
-          lastname: 'Svendsen',
-          email: 'katrine@mail.com',
-          created_at: new Date(),
-          updated_at: null
-        }
-      )
-      assert(1, r.insertedCount)
-      res.send('startpage');
+      query = {
+        state: { $in: ['Indiana','California']}
+      }
+      fields = {
+        id: 1,
+        name: 1,
+        city: 1,
+        state: 1,
+        mail: 1,
+        _id: 0
+      }
+      options = {
+        sort: { id: -1 },
+        limit: 6
+      }
+      let col = db.collection(userCollection)
+      let cursor = await col.find(query, options).project(fields).toArray()
+      res.status(200).json(cursor)
     } catch (err) {
       next(err)
     }
 
   })
-  .post(validate.post, (req, res) => {
+  .post(validate.post, async (req, res, next) => {
     res.status(201).json(req.body)
   })
   .put(validate.put, (req, res) => {
