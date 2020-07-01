752f082b82cae24e89789ab445f29bde483a2d73
Jackie <brundtoe@outlook.dk>
2020-07-01 16:47:26 +0200

    lister books med author 3

--- middleware/books/index.js
+++ middleware/books/index.js
@@ -4,7 +4,29 @@ const userCollection = 'books'
 
 module.exports = {
   async index (req, res, next) {
-    res.status(200).json({success: 'Books index function'})
+
+    try {
+      let db = mongoCon.getConnection()
+      query = {
+        author_id: { $in: [3] }
+      }
+      fields = {
+        id: 1,
+        title: 1,
+        published: 1,
+        onhand: 1,
+        _id: 0
+      }
+      options = {
+        sort: { id: -1 },
+        limit: 99
+      }
+      let col = db.collection(userCollection)
+      let cursor = await col.find(query, options).project(fields).toArray()
+      res.status(200).json({data: cursor})
+    } catch (err) {
+      next(err)
+    }
   },
   async save (req, res, next) {
     res.status(200).json({success: 'Books save function'})
