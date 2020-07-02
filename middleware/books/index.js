2f16557b8bf300fa6531e3b7aeaccae04bca871a
Jackie <brundtoe@outlook.dk>
2020-07-02 17:02:10 +0200

    list book written by author using aggregate to show author name

--- middleware/books/index.js
+++ middleware/books/index.js
@@ -8,21 +8,34 @@ module.exports = {
     try {
       let db = mongoCon.getConnection()
       query = {
-        author_id: { $in: [3] }
+        author_id: 5
       }
       fields = {
+        _id: 0,
         id: 1,
         title: 1,
         published: 1,
         onhand: 1,
-        _id: 0
+        name: { $concat: ['$author.firstname', ' ', '$author.lastname']}
       }
+
+      join = {
+        from: "authors",
+          localField: "author_id",
+          foreignField: "id",
+          as: "author"
+      }
+
       options = {
-        sort: { id: -1 },
-        limit: 99
+        sort: { id: 1 },
       }
       let col = db.collection(userCollection)
-      let cursor = await col.find(query, options).project(fields).toArray()
+      let cursor = await col.aggregate([
+        { '$match': query},
+        {'$lookup': join },
+        {'$unwind': '$author'},
+        {'$project': fields}
+      ]).toArray()
       res.status(200).json({data: cursor})
     } catch (err) {
       next(err)
