bfd0c40f4cb9914b016d2157e06220214fe00f6a
Jackie <brundtoe@outlook.dk>
2020-07-05 17:50:56 +0200

    tilføjet validering for books at author_id eksisterer for post og put requests

--- middleware/books/validate.js
+++ middleware/books/validate.js
@@ -1,27 +1,31 @@
 const createError = require('http-errors')
 const booksSchema = require('./booksSchema')
+const {authorExists} = require('../../lib/authorExists')
 const Joi = require('@hapi/joi')
 
 module.exports = {
-  post: (req, res, next) => {
-
+  post: async (req, res, next) => {
     try {
       const schema = booksSchema
         .with('title', ['author_id','bookprice','onhand'])
         .or('title','isbn','published')
       Joi.assert(req.body, schema)
+      const author = await authorExists(req.body.author_id)
+      if (!author) throw createError(400,'author does not exist')
       next()
     } catch (err) {
       next(createError(400, err))
     }
   },
-  put: (req, res, next) => {
+  put: async (req, res, next) => {
 
     try {
       const schema = booksSchema
         .with('title',['_id','id','author_id','bookprice','onhand'])
         .or('title','isbn', 'published')
       Joi.assert(req.body,schema )
+      const author = await authorExists(req.body.author_id)
+      if (!author) throw createError(400,'author does not exist')
       next()
     } catch (err) {
       next(createError(400, err))
