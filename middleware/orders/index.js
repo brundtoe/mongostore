c0923abf27824253b65e6b9ca35243630182ae94
Jackie <brundtoe@outlook.dk>
2020-07-07 17:33:57 +0200

    aktivereret orders controller index og show actions

--- middleware/orders/index.js
+++ middleware/orders/index.js
@@ -1,13 +1,22 @@
 const mongoCon = require('../../dbs')
 const createError = require('http-errors')
-const {getNextId} = require('../../lib/getNextId')
-const booksCollection = 'books'
+const { getNextId } = require('../../lib/getNextId')
+const ordersCollection = 'bookorders'
 
 module.exports = {
   async index (req, res, next) {
     try {
       let db = mongoCon.getConnection()
-      res.status(200).json({ data: { success: 'bookorders index' } })
+      const bookorders = await db.collection(ordersCollection).find().toArray()
+      const result = bookorders.map(order => {
+        return {
+          id: order.id,
+          orderdate: order.orderdate,
+          shipdate: order.shipdate,
+          link: `http://localhost:3000/orders/${order.id}`
+        }
+      })
+      res.status(200).json({ data: result })
     } catch (err) {
       next(createError(400, err.message))
     }
@@ -17,7 +26,8 @@ module.exports = {
     const order_id = parseInt(req.params.id)
     try {
       let db = mongoCon.getConnection()
-      res.status(200).json({ data: { success: 'bookorders show' } })
+      const bookorder = await db.collection(ordersCollection).findOne({ id: order_id })
+      res.status(200).json({ data: bookorder })
     } catch (err) {
       next(createError(400, err.message))
     }
