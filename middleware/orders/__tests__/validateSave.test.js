c53ca24a11b5893744679a7415b9fbdce3eda29c
Jackie <brundtoe@outlook.dk>
2020-07-07 10:54:26 +0200

    tilføejt test af at datoer er lig med eller større end orderdate

--- middleware/orders/__tests__/validateSave.test.js
+++ middleware/orders/__tests__/validateSave.test.js
@@ -80,5 +80,72 @@ describe('Validation of orders save action', () => {
     expect(next.mock.calls[0][0]).toBe(undefined)
   })
 
+  test('should fail shipdate less than orderdate', () => {
+    const req = {
+      body: {
+        user_id: 3,
+        orderdate: new Date('2020-06-03'),
+        paymethod: 'AMEX',
+        shipby: 'UPS',
+        shipdate: new Date('2020-06-01'),
+      }
+    }
+    const res = jest.fn()
+    const next = jest.fn()
+    const expected = /\"message\":\"\\\"shipdate\\\" must be greater than \\\"ref:orderdate\\\"\"/
+    validate.post(req, res, next)
+    expect(next.mock.calls.length).toBe(1)
+    expect(next.mock.calls[0][0]).toBeInstanceOf(Joi.ValidationError)
+    expect(next.mock.calls[0][0]).toMatchObject({ _original: req.body })
+    expect(next.mock.calls[0][0]).toContainKey('details')
+    expect(next.mock.calls[0][0]).toMatchObject({ status: 400 })
+    const actual = JSON.stringify(next.mock.calls[0][0])
+    expect(actual).toMatch(expected)
+  })
+
+  test('should fail paydate less than orderdate', () => {
+    const req = {
+      body: {
+        user_id: 3,
+        orderdate: new Date('2020-06-03'),
+        paymethod: 'AMEX',
+        shipby: 'UPS',
+        paydate: new Date('2020-06-01'),
+      }
+    }
+    const res = jest.fn()
+    const next = jest.fn()
+    const expected = /\"message\":\"\\\"paydate\\\" must be greater than \\\"ref:orderdate\\\"\"/
+    validate.post(req, res, next)
+    expect(next.mock.calls.length).toBe(1)
+    expect(next.mock.calls[0][0]).toBeInstanceOf(Joi.ValidationError)
+    expect(next.mock.calls[0][0]).toMatchObject({ _original: req.body })
+    expect(next.mock.calls[0][0]).toContainKey('details')
+    expect(next.mock.calls[0][0]).toMatchObject({ status: 400 })
+    const actual = JSON.stringify(next.mock.calls[0][0])
+    expect(actual).toMatch(expected)
+  })
+  test('should fail invoicedate less than orderdate', () => {
+    const req = {
+      body: {
+        user_id: 3,
+        orderdate: new Date('2020-06-03'),
+        paymethod: 'AMEX',
+        shipby: 'UPS',
+        invoicedate: new Date('2020-06-01'),
+      }
+    }
+    const res = jest.fn()
+    const next = jest.fn()
+    const expected = /\"message\":\"\\\"invoicedate\\\" must be greater than \\\"ref:orderdate\\\"\"/
+    validate.post(req, res, next)
+    expect(next.mock.calls.length).toBe(1)
+    expect(next.mock.calls[0][0]).toBeInstanceOf(Joi.ValidationError)
+    expect(next.mock.calls[0][0]).toMatchObject({ _original: req.body })
+    expect(next.mock.calls[0][0]).toContainKey('details')
+    expect(next.mock.calls[0][0]).toMatchObject({ status: 400 })
+    const actual = JSON.stringify(next.mock.calls[0][0])
+    expect(actual).toMatch(expected)
+  })
 
 })
