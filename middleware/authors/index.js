7ee5eb95e7cdbe1e73b6074264a9bf4f1a0921b1
Jackie <brundtoe@outlook.dk>
2021-11-22 16:48:33 +0100

    refaktorering af authors controller og etablering af authors models

--- middleware/authors/index.js
+++ middleware/authors/index.js
@@ -1,98 +1,79 @@
-const mongoCon = require('../../dbs')
-const authorCollection = 'authors'
-const { getNextId } = require('../../lib/getNextId')
-const createError = require('http-errors')
+const authors = require('../../models/authors')
 
 module.exports = {
   async index (req, res, next) {
 
     try {
-      let db = await mongoCon.getConnection()
-      const query = {}
-
-      const join = {
-        from: "books",
-        localField: "id",
-        foreignField: "author_id",
-        as: "bookswritten"
-      }
-
-      const fields = {
-        _id: 0,
-        id: 1,
-        name: { $concat: ['$firstname', ' ', '$lastname']},
-        mail:1,
-        'bookswritten.title':1,
-        'bookswritten.published': 1
-      }
-
-      let col = db.collection(authorCollection)
-      let cursor = await col.aggregate([
-        { '$match': query},
-        {'$lookup': join },
-        {'$project': fields},
-        {'$limit': 4}
-      ]).toArray()
-      res.status(200).json({data: cursor})
+      const data = await authors.findAll()
+      const status = data['data'] ? 200 : 404
+      res.status(status).json(data)
     } catch (err) {
-      next(createError(400, err.message))
+      next(err)
     }
   },
 
   async show (req,res,next) {
-    try{
-      const id = parseInt(req.params.id)
-      let db = await mongoCon.getConnection()
-      const author = await db.collection(authorCollection).findOne({id:id})
-      res.status(200).json({data: author})
+    const author_id = parseInt(req.params.id)
+
+    try {
+      const data = await authors.findById(author_id)
+      const status = data['data'] ? 200 : 404
+      res.status(status).json(data)
     } catch (err) {
-      next(createError(400, err.message))
+      next(err)
     }
   },
   async save (req, res, next) {
-    try {
-      const author_id = await getNextId('author_id')
-      let db = await mongoCon.getConnection()
-      req.body.id = author_id.value.next_value
-      const result = await db.collection(authorCollection).insertOne(req.body)
-      const author = await db.collection(authorCollection).findOne({id : req.body.id})
-      res.status(201).json({
-        data: {
-          acknowledged: result.acknowledged,
-          insertId: result.insertedId,
-          author: author
-        }
-      })
-    } catch (err) {
-      next(createError(400, err.message))
-    }
 
+    const author = {
+      id: null,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      mail: req.body.mail
+    }
+    try {
+      const data = await authors.save(author)
+      res.status(201).json(data)
+
+    } catch (err) {
+      next(err)
+    }
   },
   async update (req, res, next) {
     const author_id = parseInt(req.body.id)
-    try {
-      let db = await mongoCon.getConnection()
-      const result = await db.collection(authorCollection).findOneAndReplace({ id: author_id },
-        {
-          id: author_id,
-          firstname: req.body.firstname,
-          lastname: req.body.lastname,
-          mail: req.body.mail
-        },{returnDocument: 'after'})
-      res.status(201).json(result)
-    } catch (err) {
-      next(createError(400, err.message))
+
+    const author = {
+      id: author_id,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      mail: req.body.mail
     }
+
+    try {
+      const data = await authors.updateById(author)
+      const status = data['data'] ? 200 : 404
+      res.status(status).json(data)
+    } catch (err) {
+      next(err)
+    }
+
   },
   async delete (req, res, next) {
     const author_id = parseInt(req.params.id)
     try {
-      let db = await mongoCon.getConnection()
-      const result = await db.collection(authorCollection).findOneAndDelete({ id: author_id })
-      res.status(200).json({ data: result })
+      const data = await authors.deleteById(author_id)
+      let status
+      if (data.data) {
+        status = 200
+      }
+      else {
+        status = data.error.type === 'NOT_ALLOWED' ? 403 : 404
+      }
+      res.status(status).json(data)
     } catch (err) {
-      next(createError(400, err.message))
+      next(err)
     }
+
   }
 
 }
---
+++ models/authors.js
@@ -0,0 +1,133 @@
+const mongoCon = require('../dbs')
+const { getNextId } = require('../lib/getNextId')
+const authorCollection = 'authors'
+
+module.exports = {
+  async findAll () {
+
+    try {
+      let db = await mongoCon.getConnection()
+      const query = {}
+
+      const join = {
+        from: 'books',
+        localField: 'id',
+        foreignField: 'author_id',
+        as: 'bookswritten'
+      }
+
+      const fields = {
+        _id: 0,
+        id: 1,
+        name: { $concat: ['$firstname', ' ', '$lastname'] },
+        mail: 1,
+        'bookswritten.title': 1,
+        'bookswritten.published': 1
+      }
+
+      let col = db.collection(authorCollection)
+      let cursor = await col.aggregate([
+        { '$match': query },
+        { '$lookup': join },
+        { '$project': fields },
+        { '$limit': 4 }
+      ]).toArray()
+      return cursor ? { 'data': cursor } : {
+        error: {
+          type: 'RESOURCE_NOT_FOUND',
+          description: 'Authors collection findes ikke',
+        }
+      }
+    } catch(err) {
+      return action_failed(err.message)
+    }
+  },
+
+  async findById (author_id) {
+    try {
+      let db = await mongoCon.getConnection()
+      const author = await db.collection(authorCollection).findOne({ id: author_id })
+      return author ? { data: author } : author_not_found(author_id)
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+
+  async deleteById (author_id) {
+    try {
+      let db = await mongoCon.getConnection()
+      const result = await db.collection(authorCollection).findOneAndDelete({ id: author_id })
+      return (result.ok === 1) ? author_slettet(author_id) : author_not_found(author_id)
+
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+  async updateById (author) {
+    let db = await mongoCon.getConnection()
+    const result = await db.collection(authorCollection).findOneAndReplace({ id: author.id }, author, { returnDocument: 'after' })
+    return (result.ok === 1) ? author_opdateret(author.id) : author_not_found(author.id)
+  },
+
+  async save (author) {
+    try {
+      const author_id = await getNextId('author_id')
+      let db = await mongoCon.getConnection()
+      author.id = author_id.value.next_value
+      const result = await db.collection(authorCollection).insertOne(author)
+      return (result.acknowledged) ? author_oprettet : action_failed('Author save')
+
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+}
+
+/**
+ * Functions to return messages
+ */
+function author_not_found (author_id) {
+  return {
+    error: {
+      type: 'RESOURCE_NOT_FOUND',
+      description: `Author ${author_id} findes ikke`,
+    }
+  }
+}
+
+function author_slettet (author_id) {
+  return {
+    data: {
+      message: `Author ${author_id} er slettet`,
+      author_id: author_id
+    }
+  }
+}
+
+function author_opdateret(author_id){
+  return {
+    data: {
+      message: `Opdateret author ${author_id}`,
+      author_id: author_id
+    }
+  }
+}
+
+function author_oprettet(author_id) {
+  return {
+    data:
+      {
+        message: `Oprettet author ${author_id}`,
+        author_id: author_id
+      }
+  }
+}
+
+function action_failed(action) {
+  return {
+    error: {
+      type: 'RESOURCE_NOT_FOUND',
+      description: `Transaktionen ${action} fejlede`,
+    }
+  }
+}
