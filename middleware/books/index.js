e29d34219d1daaf57e2dcc57b77a92b5d159b5e7
Jackie <brundtoe@outlook.dk>
2021-11-23 15:29:34 +0100

    refaktorering af books controller og etablering af books models

--- middleware/books/index.js
+++ middleware/books/index.js
@@ -1,100 +1,88 @@
 const mongoCon = require('../../dbs')
 const createError = require('http-errors')
-const {getNextId} = require('../../lib/getNextId')
+const { getNextId } = require('../../lib/getNextId')
+const books = require('../../models/books')
+const authors = require('../../models/authors')
 const booksCollection = 'books'
 
 module.exports = {
   async index (req, res, next) {
 
     try {
-      let db = await mongoCon.getConnection()
-      const query = {
-        author_id: 5
-      }
-      const fields = {
-        _id: 0,
-        id: 1,
-        title: 1,
-        published: 1,
-        onhand: 1,
-        name: { $concat: ['$author.firstname', ' ', '$author.lastname']}
-      }
-
-      const join = {
-        from: "authors",
-          localField: "author_id",
-          foreignField: "id",
-          as: "author"
-      }
-
-      let col = db.collection(booksCollection)
-      let cursor = await col.aggregate([
-        { '$match': query},
-        {'$lookup': join },
-        {'$unwind': '$author'},
-        {'$project': fields}
-      ]).toArray()
-      res.status(200).json({data: cursor})
+      const data = await books.findAll()
+      const status = data['data'] ? 200 : 404
+      res.status(status).json(data)
     } catch (err) {
-      next(createError(400, err.message))
+      next(err)
     }
   },
 
-  async show (req,res,next)  {
+  async show (req, res, next) {
+    const book_id = parseInt(req.params.id)
+
     try {
-      const id = parseInt(req.params.id)
-      let db = await mongoCon.getConnection()
-      const book = await db.collection(booksCollection).findOne({id:id})
-      res.status(200).json({data: book })
+      const data = await books.findById(book_id)
+      const status = data['data'] ? 200 : 404
+      res.status(status).json(data)
     } catch (err) {
-      next(createError(400, err.message))
+      next(err)
     }
   },
+
   async save (req, res, next) {
-    try {
-      const book_id = await getNextId('book_id')
-      const db = await mongoCon.getConnection()
-      req.body.id = book_id.value.next_value
-      const result = await db.collection(booksCollection).insertOne(req.body)
-      const book = await db.collection(booksCollection).findOne({id : req.body.id})
-      res.status(201).json({
-        data: {
-          acknowledged: result.acknowledged,
-          insertId: result.insertedId,
-          book: book
-        }
-      })
-    } catch (err) {
-      next(createError(400, err.message))
+    const book = {
+      id: null,
+      author_id: req.body.author_id,
+      title: req.body.title,
+      published: req.body.published,
+      bookprice: req.body.bookprice,
+      isbn: req.body.isbn,
+      onhand: req.body.onhand
     }
+    try {
+      const data = await books.save(book)
+      res.status(201).json(data)
+
+    } catch (err) {
+      next(err)
+    }
+
   },
+
   async update (req, res, next) {
     const book_id = parseInt(req.body.id)
-    try {
-      const db = await mongoCon.getConnection()
-      const result = await db.collection(booksCollection).findOneAndReplace({ id: book_id },
-        {
-          id: book_id,
-          author_id: req.body.author_id,
-          title: req.body.title,
-          published: req.body.published,
-          bookprice: req.body.bookprice,
-          isbn: req.body.isbn,
-          onhand: req.body.onhand
-        },{returnDocument: 'after'})
-      res.status(201).json(result)
-    } catch (err) {
-      next(createError(400, err.message))
+    const book = {
+      id: book_id,
+      author_id: req.body.author_id,
+      title: req.body.title,
+      published: req.body.published,
+      bookprice: req.body.bookprice,
+      isbn: req.body.isbn,
+      onhand: req.body.onhand
     }
+    try {
+      const data = await books.updateById(book)
+      const status = data['data'] ? 200 : 404
+      res.status(status).json(data)
+    } catch (err) {
+      next(err)
+    }
+
   },
+
   async delete (req, res, next) {
     const book_id = parseInt(req.params.id)
     try {
-      const db = await mongoCon.getConnection()
-      const result = await db.collection(booksCollection).findOneAndDelete({ id: book_id })
-      res.status(200).json({ data: result })
+      const data = await books.deleteById(book_id)
+      let status
+      if (data.data) {
+        status = 200
+      } else {
+        status = data.error.type === 'NOT_ALLOWED' ? 403 : 404
+      }
+      res.status(status).json(data)
     } catch (err) {
-      next(createError(400, err.message))
+      next(err)
     }
   }
 
---
+++ models/books.js
@@ -0,0 +1,140 @@
+const mongoCon = require('../dbs')
+const { getNextId } = require('../lib/getNextId')
+const createError = require('http-errors')
+const booksCollection = 'books'
+
+module.exports = {
+  async findAll() {
+    try {
+      let db = await mongoCon.getConnection()
+      const query = {
+        author_id: 5
+      }
+      const fields = {
+        _id: 0,
+        id: 1,
+        title: 1,
+        published: 1,
+        onhand: 1,
+        name: { $concat: ['$author.firstname', ' ', '$author.lastname']}
+      }
+
+      const join = {
+        from: "authors",
+        localField: "author_id",
+        foreignField: "id",
+        as: "author"
+      }
+
+      let col = db.collection(booksCollection)
+      let cursor = await col.aggregate([
+        { '$match': query},
+        {'$lookup': join },
+        {'$unwind': '$author'},
+        {'$project': fields}
+      ]).toArray()
+      return cursor ? { 'data': cursor } : {
+        error: {
+          type: 'RESOURCE_NOT_FOUND',
+          description: 'Books collection findes ikke',
+        }
+      }
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+
+  async findById(book_id){
+    try {
+      let db = await mongoCon.getConnection()
+      const book = await db.collection(booksCollection).findOne({id:book_id})
+      return book ? { data: book } : book_not_found(book_id)
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+  async deleteById(book_id){
+
+    try {
+      const db = await mongoCon.getConnection()
+      const result = await db.collection(booksCollection).findOneAndDelete({ id: book_id })
+      return (result.ok === 1) ? book_slettet(book_id) : book_not_found(book_id)
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+  async updateById(book){
+    const book_id = parseInt(book.id)
+    try {
+      const db = await mongoCon.getConnection()
+      const result = await db.collection(booksCollection).findOneAndReplace({ id: book_id },book,{returnDocument: 'after'})
+      return (result.ok === 1) ? book_opdateret(book.id) : book_not_found(book.id)
+
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  },
+  async save(book){
+    try {
+      const book_id = await getNextId('book_id')
+      const db = await mongoCon.getConnection()
+      book.id = book_id.value.next_value
+      const result = await db.collection(booksCollection).insertOne(book)
+      return (result.acknowledged) ? book_oprettet : action_failed('Book save')
+
+    } catch (err) {
+      return action_failed(err.message)
+    }
+  }
+}
+
+
+
+/**
+ * Functions to return messages
+ */
+function book_not_found (book_id) {
+  return {
+    error: {
+      type: 'RESOURCE_NOT_FOUND',
+      description: `Book ${book_id} findes ikke`,
+    }
+  }
+}
+
+function book_slettet (book_id) {
+  return {
+    data: {
+      message: `Book ${book_id} er slettet`,
+      book_id: book_id
+    }
+  }
+}
+
+function book_opdateret(book_id){
+  return {
+    data: {
+      message: `Opdateret book ${book_id}`,
+      book_id: book_id
+    }
+  }
+}
+
+function book_oprettet(book_id) {
+  return {
+    data:
+      {
+        message: `Oprettet book ${book_id}`,
+        book_id: book_id
+      }
+  }
+}
+
+function action_failed(action) {
+  return {
+    error: {
+      type: 'RESOURCE_NOT_FOUND',
+      description: `Transaktionen ${action} fejlede`,
+    }
+  }
+}
