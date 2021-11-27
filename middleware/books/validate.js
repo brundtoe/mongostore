995a237e2e893858b7c9667f15475a07c1f83c4e
Jackie <brundtoe@outlook.dk>
2021-11-27 15:50:31 +0100

    validate book tilføjet validering af datoens gyldighed

--- middleware/books/validate.js
+++ middleware/books/validate.js
@@ -1,13 +1,14 @@
 const createError = require('http-errors')
 const booksSchema = require('./booksSchema')
 const Joi = require('joi')
-const { buildErrorMessages } = require('../../lib/buildMessages')
+const isDateValid = require('../../lib/validateDate')
+const { buildErrorMessages, invalidDateMessage } = require('../../lib/buildMessages')
 
 module.exports = {
   post: async (req, res, next) => {
     try {
       Joi.assert(req.body, booksSchema, {stripUnknown: true, abortEarly: false})
-      next()
+      isDateValid(req.body.published) ? next() : next(createError(400, invalidDateMessage(req.body.published)))
     } catch (err) {
       next(createError(400, buildErrorMessages(err,'VALIDATION_ERROR')))
     }
@@ -16,7 +17,7 @@ module.exports = {
 
     try {
       Joi.assert(req.body,booksSchema, {stripUnknown: true, abortEarly: false} )
-      next()
+      isDateValid(req.body.published) ? next() : next(createError(400, invalidDateMessage(req.body.published)))
     } catch (err) {
       next(createError(400, buildErrorMessages(err,'VALIDATION_ERROR')))
     }
@@ -40,8 +41,5 @@ module.exports = {
       next(createError(400, buildErrorMessages(err,'VALIDATION_ERROR')))
     }
   }
-
 }
-
-
 
