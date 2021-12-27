8e75694bc6fe4753601f439bdfc3731a5a7c0698
Jackie <brundtoe@outlook.dk>
2021-12-27 18:16:46 +0100

    installerede cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -15,9 +16,10 @@ const orderlinesRouter = require('./routes/orderlines')
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
