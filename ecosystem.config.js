34514906cedd19c01d277009e9e9dac4fce45dc8
Jackie <brundtoe@outlook.dk>
2022-01-08 12:07:30 +0100

    opdateret pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
   apps: [{
       name: 'mongostore',
-      cwd: '/home/vagrant/code/mongostore',
-      script: '/home/vagrant/code/mongostore/bin/www',
+      cwd: '/var/www/html/mongostore',
+      script: '/var/www/html/mongostore/bin/www',
       instances: 1,
       autorestart: true,
-      watch: true,
-    },
-    {
-      name: 'nginx start',
-      cwd: '/usr/bin',
-      script: 'systemctl restart nginx',
-      instances: 1,
-      autorestart: false,
       watch: true,
     }
   ]
