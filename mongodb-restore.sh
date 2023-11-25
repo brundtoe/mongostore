8bc98e46acec0892bfaed528acda3aadb73af889
Jackie <brundtoe@outlook.dk>
2023-11-25 16:04:56 +0100

    mongodb restore, container ændret fra mongodb til bookstore

--- mongodb-restore.sh
+++ mongodb-restore.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -i --user jackie -w /docker-entrypoint-initdb.d mongodb /bin/bash -c "bash 01-load-bookstore.sh"
+docker exec -i --user jackie -w /docker-entrypoint-initdb.d bookstore /bin/bash -c "bash 01-load-bookstore.sh"
